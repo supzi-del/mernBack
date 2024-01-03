37c04d4ddd01db9fba30ecdf2f68327ef99bc893
supzi-del <78655439+supzi-del@users.noreply.github.com>
2024-01-03 14:49:19 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,65 @@
+import express from "express";
+import cors from "cors";
+import { connectDB } from "./DB/Database.js";
+import bodyParser from "body-parser";
+import dotenv from "dotenv";
+import helmet from "helmet";
+import morgan from "morgan";
+import transactionRoutes from "./Routers/Transactions.js";
+import userRoutes from "./Routers/userRouter.js";
+import path from "path";
+
+dotenv.config({ path: "./config/config.env" });
+const app = express();
+
+const port = process.env.PORT;
+
+connectDB();
+
+const allowedOrigins = [
+  "https://main.d1sj7cd70hlter.amplifyapp.com",
+  "https://expense-tracker-app-three-beryl.vercel.app",
+  "http://localhost:3000", // Add the local development origin
+  // add more origins as needed
+];
+
+// Middleware
+app.use(express.json());
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      // Allow requests with no origin (like mobile apps or curl requests)
+      if (!origin) return callback(null, true);
+
+      if (allowedOrigins.indexOf(origin) === -1) {
+        const msg =
+          "The CORS policy for this site does not allow access from the specified origin.";
+        return callback(new Error(msg), false);
+      }
+
+      return callback(null, true);
+    },
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"], // Add headers as needed
+    exposedHeaders: ["Content-Type", "Authorization"], // Expose headers to frontend
+    optionsSuccessStatus: 204, // Set the HTTP status code for successful preflight requests
+  })
+);
+app.use(helmet());
+app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
+app.use(morgan("dev"));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// Router
+app.use("/api/v1", transactionRoutes);
+app.use("/api/auth", userRoutes);
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(port, () => {
+  console.log(`Server is listening on http://localhost:${port}`);
+});
